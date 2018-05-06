0d327e7d4a52fa0a556854ff998f2c218962bc18
Oleksandr Zhurba <runok.al@gmail.com>
2018-05-06 18:58:00 +0300

    first commit

---
+++ src/test/java/exam052018Proj/AppTest.java
@@ -0,0 +1,20 @@
+package exam052018Proj;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
